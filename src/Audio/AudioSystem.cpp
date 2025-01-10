09d09bf3e3f0663188125fba9aff2b41677fd7d8
Arthur <bebrusponuhanus@gmail.com>
2025-01-11 00:13:43 +0400

    Update AudioSystem.cpp

--- src/Audio/AudioSystem.cpp
+++ src/Audio/AudioSystem.cpp
@@ -1,60 +1,87 @@
 #include "AudioSystem.hpp"
-#include <stdexcept>
 
-AudioSystem::AudioSystem() : m_System(nullptr) {}
+AudioSystem::AudioSystem() : m_Initialized(false), m_MasterVolume(1.0f) {
+#ifdef USE_FMOD
+    m_System = nullptr;
+#endif
+}
 
 AudioSystem::~AudioSystem() {
     Shutdown();
 }
 
 bool AudioSystem::Initialize() {
-    FMOD_RESULT result;
-    result = FMOD::System_Create(&m_System);
+    if (m_Initialized) return true;
+    
+#ifdef USE_FMOD
+    FMOD_RESULT result = FMOD::System_Create(&m_System);
     if (result != FMOD_OK) {
         return false;
     }
 
-    result = m_System->init(MAX_CHANNELS, FMOD_INIT_NORMAL, nullptr);
+    result = m_System->init(32, FMOD_INIT_NORMAL, nullptr);
     if (result != FMOD_OK) {
         return false;
     }
+#endif
 
+    m_Initialized = true;
     return true;
 }
 
 void AudioSystem::Update() {
+    if (!m_Initialized) return;
+    
+#ifdef USE_FMOD
     if (m_System) {
         m_System->update();
     }
+#endif
 }
 
 void AudioSystem::Shutdown() {
+    if (!m_Initialized) return;
+    
+#ifdef USE_FMOD
     if (m_System) {
         m_System->release();
         m_System = nullptr;
     }
+#endif
+
+    m_Initialized = false;
+}
+
+bool AudioSystem::LoadSound(const std::string& filename) {
+    if (!m_Initialized) return false;
+    
+#ifdef USE_FMOD
+    // TODO: Реализовать загрузку звука через FMOD
+#endif
+
+    return true;
 }
 
 void AudioSystem::PlaySound(const std::string& filename) {
-    if (!m_System) return;
-
-    FMOD::Sound* sound = nullptr;
-    FMOD::Channel* channel = nullptr;
+    if (!m_Initialized) return;
     
-    FMOD_RESULT result = m_System->createSound(filename.c_str(), FMOD_DEFAULT, nullptr, &sound);
-    if (result == FMOD_OK) {
-        m_System->playSound(sound, nullptr, false, &channel);
-        sound->release(); // FMOD будет управлять памятью сам
-    }
+#ifdef USE_FMOD
+    // TODO: Реализовать воспроизведение звука через FMOD
+#endif
 }
 
-void AudioSystem::StopAllSounds() {
-    if (!m_System) return;
+void AudioSystem::StopSound(const std::string& filename) {
+    if (!m_Initialized) return;
     
-    FMOD::Channel* channel = nullptr;
-    for (int i = 0; i < MAX_CHANNELS; i++) {
-        if (m_System->getChannel(i, &channel) == FMOD_OK && channel) {
-            channel->stop();
-        }
-    }
+#ifdef USE_FMOD
+    // TODO: Реализовать остановку звука через FMOD
+#endif
+}
+
+void AudioSystem::SetVolume(float volume) {
+    m_MasterVolume = volume;
+    
+#ifdef USE_FMOD
+    // TODO: Реализовать установку громкости через FMOD
+#endif
 } 
