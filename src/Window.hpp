bdf7e82c149b3f4e4583abb3bb7874281151f698
Arthur <bebrusponuhanus@gmail.com>
2025-01-10 05:11:34 +0400

    Create Window.hpp

---
+++ src/Window.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+#include <GLFW/glfw3.h>
+
+class Window {
+public:
+    Window();
+    ~Window();
+
+    bool Initialize(const std::string& title, int width, int height);
+    void Update();
+    void Shutdown();
+
+    bool ShouldClose() const;
+    GLFWwindow* GetNativeWindow() const { return m_Window; }
+
+private:
+    GLFWwindow* m_Window;
+    std::string m_Title;
+    int m_Width;
+    int m_Height;
+}; 
