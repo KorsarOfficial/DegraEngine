2f108717ab89dc89f59062cc16432138f8b39d7a
Arthur <bebrusponuhanus@gmail.com>
2025-01-11 00:13:44 +0400

    Update AudioSystem.hpp

--- src/Audio/AudioSystem.hpp
+++ src/Audio/AudioSystem.hpp
@@ -1,7 +1,9 @@
 #pragma once
-
 #include <string>
+
+#ifdef USE_FMOD
 #include <fmod.hpp>
+#endif
 
 class AudioSystem {
 public:
@@ -12,10 +14,17 @@ public:
     void Update();
     void Shutdown();
 
+    // Базовые функции для работы со звуком
+    bool LoadSound(const std::string& filename);
     void PlaySound(const std::string& filename);
-    void StopAllSounds();
+    void StopSound(const std::string& filename);
+    void SetVolume(float volume);
 
 private:
+    bool m_Initialized;
+    float m_MasterVolume;
+
+#ifdef USE_FMOD
     FMOD::System* m_System;
-    static const int MAX_CHANNELS = 32;
+#endif
 }; 
